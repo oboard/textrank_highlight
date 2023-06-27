481d099af2be63ba719f97abd5c246e7e0f50371
oboard <oboard@outlook.com>
2023-06-27 20:53:57 +0800

    init

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,137 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState } from "react";
+import { Segment, useDefault } from 'segmentit';
 
-const inter = Inter({ subsets: ['latin'] })
+const useLocalStorage = (storageKey, fallbackState) => {
+  if (typeof window !== "undefined") {
+    const [value, setValue] = useState(
+      JSON.parse(localStorage.getItem(storageKey)) || fallbackState
+    );
+
+    useEffect(() => {
+      localStorage.setItem(storageKey, JSON.stringify(value));
+    }, [value, storageKey]);
+    return [value, setValue];
+  }
+  return useState(fallbackState);
+};
+
 
 export default function Home() {
+  
+  const [questions, setQuestions] = useLocalStorage("questions", "");
+  const [results, setResults] = useLocalStorage("results", "");
+
+
+  const deal = () => {
+    // 对题库进行中文分词处理
+    // const segmentit = new Segment();
+    const segmentit = useDefault(new Segment());
+    const segmentResult = segmentit.doSegment(questions);
+    // 统计每个词的数量
+    const wordCount = {};
+    segmentResult.forEach((item) => {
+      if (wordCount[item.w] === undefined) {
+        wordCount[item.w] = 1;
+      } else {
+        wordCount[item.w] += 1;
+      }
+    });
+    // 题目按照序号分割
+    const list = questions.split(/\n\d+、/g).filter((item) => item !== "" && item !== "、");
+    
+    // 每一道题都要有两个关键词，关键词按照数量排序，取最少出现的两个，添加高亮html element
+    let rawResults = "";
+    // list.forEach((item, index) => {
+      // 改成for
+    for (let index = 0; index < list.length; index++) {
+      // const item = list[index];
+      // 只对第一行高亮
+      const item = list[index].split("\n")[0];
+      // 对每一道题进行分词
+      const segmentResult = segmentit.doSegment(item);
+      // 统计每个词的数量
+      const wordCount = {};
+      segmentResult.forEach((item) => {
+        // 不统计标点符号
+        if (item.p === 2048) { // 2048是标点符号的词性
+          return;
+        }
+
+        if (wordCount[item.w] === undefined) {
+          wordCount[item.w] = 1;
+        } else {
+          wordCount[item.w] += 1;
+        }
+      });
+      // 对词频进行排序
+      const wordCountArr = Object.entries(wordCount).sort(
+        (a, b) => a[1] - b[1]
+      );
+      // 取最少出现的两个词
+      const keyword = wordCountArr.slice(0, 2).map((item) => item[0]);
+      // 高亮处理
+      const keywordReg = new RegExp(keyword.join("|").replace(/\(/g, "\\(").replace(/\)/g, "\\)"), "g");
+      
+      const result = item.replace(
+        keywordReg,
+        // 要荧光笔的效果
+        (match) => `<span style='background-color: rgb(253, 224, 71)'>${match}</span>`
+      );
+      // 拼接后面几行
+      const otherLines = list[index].split("\n").slice(1).join("\n");
+      // 拼接结果
+      rawResults += `${index + 1}、${result}\n${otherLines}\n`;
+    }
+    rawResults = rawResults.replace(/\n/g, "<br />");
+    setResults(rawResults);
+    console.log(results);
+  }
+    
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={`flex flex-col items-center justify-center`}>
+        <h1 className={`text-6xl font-bold`}>题库关键词高亮工具</h1>
+        <p className={`text-2xl mt-4`}>将题库中的关键词高亮显示</p>
+        <button className="w-64 btn mt-4" onClick={deal}>
+          高亮处理
+        </button>
+      </div>
+      {/* 左右为 daisyUI 卡片样式，左侧是原题库文本框，右侧是光亮后的文本，1:1 */}
+      <div className={`flex flex-col md:flex-row w-full mt-8 flex-1 gap-2`}>
+        <div className={`card shadow-lg w-full md:w-1/2 bg-base-100`}>
+          <div className={`card-body`}>
+            <h2 className={`card-title`}>原题库文本框</h2>
+            <textarea
+              className={`form-control textarea h-full textarea-bordered`}
+              spellCheck={false}
+              value={questions}
+              onChange={(e) => {
+                setQuestions(e.target.value);
+              }}
+              placeholder={`请输入题库文本`}
+            ></textarea>
+          </div>
+        </div>
+        {/* <div className={`card shadow-lg w-full md:w-1/2 mt-4 md:mt-0`}> */}
+        {/* 换成daisyui主题底色 */}
+        <div className={`card shadow-lg w-full md:w-1/2 mt-4 md:mt-0 bg-base-100`}>
+          <div className={`card-body`}>
+            <h2 className={`card-title`}>高亮后的文本</h2>
+            {/* 展示results中的html */}
+            <div
+              className={`textarea h-full textarea-bordered`}
+              dangerouslySetInnerHTML={{ __html: results }}
+            ></div>
+            
+            
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      
     </main>
   )
 }
